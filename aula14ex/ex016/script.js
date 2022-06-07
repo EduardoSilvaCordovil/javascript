cc09680e0721b8923b320b7b09256cef92b654ad
EduardoSilvaCordovil <86244886+EduardoSilvaCordovil@users.noreply.github.com>
2022-06-07 20:10:19 -0300

    Initial commit

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,19 +1,31 @@
 function contar() {
-    let inicio = document.getElementById('txti')
-    let fim = document.getElementeById('txtf')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementeById('txtf')//[ERRO]
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
 
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        //window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     } else {
-        res.innerHTML = 'Contando'
-        let i = Number(inicio.value)
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for (let c = i; c <= f; c <= p) {
-            res.innerHTML += `${c} \u{1f449}` 
+        if(p < 0) {
+            window.alert('Passo inválido!')
         }
-    }
+        if (i < f) {
+            //Contagem crescente
+            for(let c = 1; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`   
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }     
 } 
